cae9351f657fa1589d73ac42b9523caa2cc97ca3
JackKiefer <jack.c.kiefer@gmail.com>
2017-11-17 13:16:18 -0700

    Add documentation

--- language.py
+++ language.py
@@ -6,9 +6,18 @@ from nltk.corpus import cmudict
 
 d = cmudict.dict()
 
+"""
+Count the number of sylables in a word.
+Returns a list of integers with each integer the syllable
+count for a certain pronunciation.
+"""
 def numSyls(word):   
     return [len(list(y for y in x if y[-1].isdigit())) for x in d[word.lower()]]
 
+"""
+Generate a list of rhymes for a given word.
+"Level" is how good the rhymes should be. 
+"""
 def rhymeList(inp, level):
     entries = cmudict.entries()
     syllables = [(word, syl) for word, syl in entries if word == inp]
@@ -17,6 +26,9 @@ def rhymeList(inp, level):
         rhymes += [word for word, pron in entries if pron[-level:] == syllable[-level:]]
     return set(rhymes)
 
+"""
+Check to see if two words rhyme
+"""
 def checkRhyme( word1, word2 ):
     # first, we don't want to report 'glue' and 'unglue' as rhyming words
     # those kind of rhymes are LAME
@@ -26,6 +38,9 @@ def checkRhyme( word1, word2 ):
         return False
     return word1 in rhyme ( word2, 1 )
 
+"""
+Check to see if a list of words follows iambic pentameter
+"""
 def isIP(stanza):
     sylCount = 0;
     for word in stanza:
